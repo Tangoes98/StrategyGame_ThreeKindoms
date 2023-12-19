cd87341f2aed0c62707394278a2bfc66289c1ee7
Tangoes98 <cyriltan985@gmail.com>
2023-12-18 19:45:36 -0500

    Update UnitGirdPosition and GridData

--- Assets/_Dec_23_Optimization/_Scripts/GridSystem/OutScene/T_GridData.cs
+++ Assets/_Dec_23_Optimization/_Scripts/GridSystem/OutScene/T_GridData.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class T_GridData
 {
     T_GirdPosition _gridPosition;
+    List<T_Unit> _unitList;
 
-    public T_GirdPosition GridPosition {get { return _gridPosition;}}
+    public T_GirdPosition GridPosition { get { return _gridPosition; } }
+
+
 
 
     public T_GridData(T_GirdPosition gp)
     {
         this._gridPosition = gp;
-
+        this._unitList = new();
 
 
 
@@ -20,7 +23,25 @@ public class T_GridData
 
     public override string ToString()
     {
-        return _gridPosition.ToString();
+        string unitString = "";
+        foreach (T_Unit item in _unitList)
+        {
+            unitString += item + "\n";
+        }
+
+        return _gridPosition.ToString()
+            + "\n"
+            + unitString;
+    }
+
+
+    public void AddUnit(T_Unit unit)
+    {
+        _unitList.Add(unit);
+    }
+    public void RemoveUnit(T_Unit unit)
+    {
+        _unitList.Remove(unit);
     }
 
 }
--- Assets/_Dec_23_Optimization/_Scripts/Unit/T_Unit.cs
+++ Assets/_Dec_23_Optimization/_Scripts/Unit/T_Unit.cs
@@ -9,19 +9,53 @@ public class T_Unit : MonoBehaviour
     // unit data has to update to grid data(position, ...)
 
     T_LevelGridManager _levelGridManagerInstance;
+    T_GirdPosition _oldGridPosition;
+    T_GirdPosition _currentGridPosition;
+    T_GridData _oldGridData;
+    T_GridData _currentGridData;
+
     void Start()
     {
         _levelGridManagerInstance = T_LevelGridManager.Instance;
+        FirstStartSetUp();
+
     }
 
     void Update()
     {
         UpdateUnitGridPosition();
+        UpdateGridPositionData();
+
+    }
+
+#region UpdateUnitGirdPosition and GridData
+    
+    void FirstStartSetUp()
+    {
+        UpdateUnitGridPosition();
+        _oldGridPosition = _currentGridPosition;
+        _currentGridData = CurrentGridData(_currentGridPosition);
+        _oldGridData = _currentGridData;
+        _currentGridData.AddUnit(this);
     }
 
     void UpdateUnitGridPosition()
     {
-        T_GirdPosition girdPosition = _levelGridManagerInstance.WorldToGridPosition(this.transform.position);
-        this.transform.position = _levelGridManagerInstance.GridToWorldPosition(girdPosition);
+        _currentGridPosition = _levelGridManagerInstance.WorldToGridPosition(this.transform.position);
+        this.transform.position = _levelGridManagerInstance.GridToWorldPosition(_currentGridPosition);
     }
+
+    void UpdateGridPositionData()
+    {
+        if (_currentGridPosition == _oldGridPosition) return;
+        _oldGridData.RemoveUnit(this);
+        _currentGridData = CurrentGridData(_currentGridPosition);
+        _currentGridData.AddUnit(this);
+        _oldGridData = _currentGridData;
+        _oldGridPosition = _currentGridPosition;
+    }
+
+    T_GridData CurrentGridData(T_GirdPosition gp) => _levelGridManagerInstance.GetCurrentGridPositionData(gp);
+    
+#endregion
 }
